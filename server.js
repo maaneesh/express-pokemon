66992c8bc18e550e796fe0efe075296c87c2a69c
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-08-19 23:52:12 -0400

    upadated output

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const port = 5002;
 
-app.get('/',(req,res)=>{
-    res.send(`
+const pokemon = require("./models/pokemon")
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+app.get("/", (req, res) => {
+  res.send(`
     <h1>Welcome to the Pokemon App!</h1>
+    `);
+});
 
-    `)
-})
+app.get("/pokemon", (req, res) => {
+  res.render("/Index",{
+    pokemons: pokemon
 
+  });
+});
 
-
-app.listen(port,()=>{
-    console.log(`Listening on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
---
+++ views/Show.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Show({pokemons}) {
+  return (
+    <div>Show</div>
+  )
+}
+
+module.exports = Show
